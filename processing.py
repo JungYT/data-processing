54c7cef042bd001b609677a9b0e86aef533aa6f5
Yeontaek Jung <skfmsgka@snu.ac.kr>
2022-11-15 14:19:59 +0900

    3rd version
    1. Add standard deviation error graph

--- processing.py
+++ processing.py
@@ -39,17 +39,22 @@ args = parser.parse_args()
 
 df = pd.read_csv(args.d, header=0, index_col=0)
 
-time_mus = df["timestamp_sample"].to_numpy()
-time = (time_mus - time_mus[0]) / 1e6
-x = df["x"].to_numpy()
-y = df["y"].to_numpy()
-z = df["z"].to_numpy()
-vx = df["vx"].to_numpy()
-vy = df["vy"].to_numpy()
-vz = df["vz"].to_numpy()
-ax = df["ax"].to_numpy()
-ay = df["ay"].to_numpy()
-az = df["az"].to_numpy()
+time_mus = df["timestamp_sample"].to_numpy() # Time since system start (microseconds)
+time = (time_mus - time_mus[0]) / 1e6 # Time since data is logged (seconds) 
+x = df["x"].to_numpy() # North position in NED earth-fixed frame (meter)
+y = df["y"].to_numpy() # East position in NED earth-fixed frame (meter)
+z = df["z"].to_numpy() # Down position in NED earth-fixed frame (meter)
+vx = df["vx"].to_numpy() # North position in NED earth-fixed frame (meter/sec)
+vy = df["vy"].to_numpy() # East position in NED earth-fixed frame (meter/sec)
+vz = df["vz"].to_numpy() # Down position in NED earth-fixed frame (meter/sec)
+ax = df["ax"].to_numpy() # North position in NED earth-fixed frame (meter/sec^2)
+ay = df["ay"].to_numpy() # East position in NED earth-fixed frame (meter/sec^2)
+az = df["az"].to_numpy() # Down position in NED earth-fixed frame (meter/sec^2)
+heading = df["heading"].to_numpy() # Euler yaw angle transforming the tarngent plane relative to NED earth-fixed frame, [-PI ~ PI] (radians)
+std_pos_horizontal = df['eph'].to_numpy() # Standard devitation of horizontal position error (meter)
+std_pos_vertical = df['epv'].to_numpy() # Standard devitation of vertical position error (meter)
+std_vel_horizontal = df['evh'].to_numpy() # Standard devitation of horizontal velocity error (meter/sec)
+std_vel_vertical = df['evv'].to_numpy() # Standard devitation of horizontal velocity error (meter/sec)
 
 
 Path("results", args.d).mkdir(parents=True, exist_ok=True)
@@ -71,6 +76,8 @@ true_color = "r"
 ref_color = "b"
 cmd_color = "k"
 
+
+# Trajectory
 fig, ax = plt.subplots(1, 1, subplot_kw={"projection": "3d"})
 ax.plot(
     x,
@@ -89,6 +96,27 @@ if args.save:
     fig.savefig(Path(path_saved, "trajectory.png"), bbox_inches="tight")
 plt.close("all")
 
+# Position
+fig, ax = plt.subplots(3, 1)
+ax[0].plot(time, x, color=true_color, linestyle=true_style, label="true")
+# ax[0].plot(t, vel_ref[:, 0], color=ref_color, linestyle=ref_style, label="ref.")
+ax[0].set_ylabel("X (m)")
+ax[0].axes.xaxis.set_ticklabels([])
+ax[1].plot(time, y, color=true_color, linestyle=true_style)
+# ax[1].plot(t, vel_ref[:, 1], color=ref_color, linestyle=ref_style)
+ax[1].set_ylabel("Y (m)")
+ax[1].axes.xaxis.set_ticklabels([])
+ax[2].plot(time, z, color=true_color, linestyle=true_style)
+# ax[2].plot(t, vel_ref[:, 2], color=ref_color, linestyle=ref_style)
+ax[2].set_ylabel("Z (m)")
+ax[0].legend()
+fig.suptitle("Position")
+fig.supxlabel("Time (s)")
+fig.align_ylabels(ax)
+fig.tight_layout()
+if args.save:
+    fig.savefig(Path(path_saved, "position.png"), bbox_inches="tight")
+plt.close("all")
 
 # Velocity
 fig, ax = plt.subplots(3, 1)
@@ -110,6 +138,39 @@ fig.align_ylabels(ax)
 fig.tight_layout()
 if args.save:
     fig.savefig(Path(path_saved, "velocity.png"), bbox_inches="tight")
+plt.close("all")
+
+# Heading angle
+fig, ax = plt.subplots(1, 1)
+ax.plot(time, heading * 180 / np.pi, color=true_color, linestyle=true_style, label='true')
+ax.set_ylabel('Heading angle (deg)')
+ax.set_xlabel('Time (s)')
+fig.tight_layout()
+if args.save:
+    fig.savefig(Path(path_saved, "heading.png"), bbox_inches="tight")
+plt.close("all")
+
+# Standard deivation
+fig, ax = plt.subplots(2, 2)
+ax[0][0].plot(time, std_pos_horizontal, color=true_color, linestyle=true_style, label="true")
+ax[0][0].set_ylabel("std pos horizontal (m)")
+ax[0][0].axes.xaxis.set_ticklabels([])
+ax[0][1].plot(time, std_pos_vertical, color=true_color, linestyle=true_style, label="true")
+ax[0][1].set_ylabel("std pos vertical (m)")
+ax[0][1].axes.xaxis.set_ticklabels([])
+
+
+ax[1][0].plot(time, std_vel_horizontal, color=true_color, linestyle=true_style)
+ax[1][0].set_ylabel("td vel horizontal (m/s)")
+ax[1][1].plot(time, std_vel_vertical, color=true_color, linestyle=true_style)
+ax[1][1].set_ylabel("td vel vertical (m/s)")
+ax[0][1].legend()
+fig.suptitle("Standard devitation of position and velocity error")
+fig.supxlabel("Time (s)")
+fig.align_ylabels(ax)
+fig.tight_layout()
+if args.save:
+    fig.savefig(Path(path_saved, "std.png"), bbox_inches="tight")
 plt.close("all")
 
 breakpoint()
